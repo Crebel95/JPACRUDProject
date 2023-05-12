e0eb4fec84fc074a972d83bec065860104993b32
Jacob <jwv928@gmail.com>
2023-05-12 19:08:43 -0400

    createspring boot mvc project

---
+++ SilverCoins/src/main/java/com/skilldistillery/silvercoins/SilverCoinsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.silvercoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverCoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverCoinsApplication.class, args);
+	}
+
+}
---
+++ SilverCoins/src/test/java/com/skilldistillery/silvercoins/SilverCoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.silvercoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverCoinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
