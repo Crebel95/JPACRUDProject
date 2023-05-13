477e967daf5c443db2379aadb249c748d9cb5d37
Jacob <jwv928@gmail.com>
2023-05-12 22:02:33 -0400

    completed silverCoin constructor

--- JPASilverCoins/src/main/java/com/skilldistillery/silvercoins/entities/SilverCoin.java
+++ JPASilverCoins/src/main/java/com/skilldistillery/silvercoins/entities/SilverCoin.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.silvercoins.entities;
 
+import java.time.Year;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,9 +21,30 @@ public class SilverCoin {
 	private String type;
 
 	private String mint;
+	
+	private Year date;
+	
+	private Double value;
+	
+	@Column(name="silver_content_grams")
+	private Double silverContent;
 
+	@Column(name="total_weight_grams")
+	private Double weight;
+	
 	public SilverCoin() {
 		super();
+	}
+
+	public SilverCoin(int id, String type, String mint, Year date, Double value, Double silverContent, Double weight) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.mint = mint;
+		this.date = date;
+		this.value = value;
+		this.silverContent = silverContent;
+		this.weight = weight;
 	}
 
 	public int getId() {
@@ -47,9 +71,44 @@ public class SilverCoin {
 		this.mint = mint;
 	}
 
+	public Year getDate() {
+		return date;
+	}
+
+	public void setDate(Year date) {
+		this.date = date;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public Double getSilverContent() {
+		return silverContent;
+	}
+
+	public void setSilverContent(Double silverContent) {
+		this.silverContent = silverContent;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
 	@Override
 	public String toString() {
-		return "SilverCoin [id=" + id + ", type=" + type + ", mint=" + mint + "]";
+		return "SilverCoin [id=" + id + ", type=" + type + ", mint=" + mint + ", date=" + date + ", value=" + value
+				+ ", silverContent=" + silverContent + ", weight=" + weight + "]";
 	}
+
+
 
 }
